3237437951031e138bdc871742afb2171e9fd902
S4Selenium <47279496+Nithinps021@users.noreply.github.com>
2022-02-28 12:00:02 +0530

    DataDFM

--- dataDrivenFrameDB.java
+++ dataDrivenFrameDB.java
@@ -1,27 +1,32 @@
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class dataDrivenFrameDB {
-
-	public static void main(String[] args) throws Exception {
+	
+	public ArrayList<String> getData(String Testcase) throws Exception
+	{
+		ArrayList<String> a =new ArrayList<String>();
 		// TODO Auto-generated method stub
 		 FileInputStream fil=new FileInputStream("D:\\New folder\\DataDriven.xlsx");
 		 XSSFWorkbook wb = new  XSSFWorkbook(fil);
 		 int sheets =0;
 		 //Accessing the sheets of the workbook
 		 sheets = wb.getNumberOfSheets();
-	     System.out.println(sheets);
+	    // System.out.println(sheets);
 	     //iterating through all the sheets and finding the required one
 	     for(int i=0;i<sheets;i++)
 	     {   
 	    	 //fetching the required sheet
-	    	 if(wb.getSheetName(i).equalsIgnoreCase("TestData"))
+	    	 if(wb.getSheetName(i).equalsIgnoreCase("Smoke"))
 	    	 {
 	    		 //working on the right sheeet
 	    		XSSFSheet sheet = wb.getSheetAt(i);
@@ -44,12 +49,43 @@ public class dataDrivenFrameDB {
 	    			}
 	    			k++;
 	    		}  
-	    		System.out.println(col);
+	    		//System.out.println(col);
 	    		//now scan the entire row and get all the deatils in the row
+	    		while(rows.hasNext())
+	    		{
+	    			Row r = rows.next();
+	                    
+	    			if(r.getCell(col).getStringCellValue().equalsIgnoreCase(Testcase))
+	    			{
+	    				 Iterator<Cell> cell = r.cellIterator();
+	    				 cell.next();
+	    				 while(cell.hasNext())
+	    					//type = string 
+	    				 {
+	    				    a.add(cell.next().getStringCellValue());
+	    				    
+	    				 }
+	    				 
+	    				 //case 2 numeric
+	    				    
+	    				 a.add(NumberToTextConverter.toText(cell.next().getNumericCellValue()));
+	    				 
+	    				 
+	    			}
+	    				
+	    			
+	    		}
 	    	 } 
 	    	 
 	     }
+		return a;
+	}
+
+	public static void main(String[] args) throws Exception {
+		
 	     
 	}
 
 }
+
+
